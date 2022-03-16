70f7cf68ee040b3525eda686921a603a8dbed73a
tibucuartas <tibucuartas@gmail.com>
2022-03-15 19:35:42 -0500

    Created UI components
    started working on the description section of each role of the application.

--- postulateudem/src/App.js
+++ postulateudem/src/App.js
@@ -6,6 +6,7 @@ import styled from 'styled-components';
 //components
 import { AccountBox } from './components/AccountBox';
 import { Navigation } from './components/Navigation';
+import { StudentBlob } from './components/RoleDescription/blobs';
 
 const AppContainer = styled.div`
 width: 100%;
@@ -16,10 +17,47 @@ align-items: center;
 justify-content: center;
 `;
 
+const DataContainer = styled.div`
+width: 100%;
+height: 80%;
+display: flex;
+flex-direction: row;
+align-items: center;
+justify-content: center;
+`;
+
+const InputSection = styled.div`
+width: 50%;
+height: 100%;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+`;
+
+const InfoSection = styled.div`
+width: 50%;
+height: 100%;
+display: flex;  
+flex-direction: row;
+align-items: center;
+`;
+
+
 function App() {
   return <AppContainer>
-            <Navigation/>
-            <AccountBox />
+           <Navigation/>
+            
+          <DataContainer>
+              <InputSection>
+                <AccountBox />
+              </InputSection>
+
+              <InfoSection>
+                <StudentBlob/>
+              </InfoSection>
+          </DataContainer>
+            
          </AppContainer>;
 }
 
---
+++ postulateudem/src/components/RoleDescription/studentBlob.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const StudentBlob = styled.div`
+width: 500px;
+height: 500px;
+background-size: cover;
+background-repeat: no-repeat;
+background-image: url("data:image/svg+xml;utf8, %3Csvg width=%22100%25%22 height=%22100%25%22 viewBox=%220 0 1000 1000%22 xmlns=%22http:%2F%2Fwww.w3.org%2F2000%2Fsvg%22 %3E %3Cdefs%3E %3CradialGradient id=%22radialGradientId%22 r=%22100%25%22 cx=%2250%25%22 cy=%2250%25%22 %3E %3Cstop offset=%220%25%22 stop-color=%22%23FF0037%22 %2F%3E %3Cstop offset=%22100%25%22 stop-color=%22%23ff90a8%22 %2F%3E %3C%2FradialGradient%3E %3CclipPath id=%22shape%22%3E %3Cpath fill=%22currentColor%22 d=%22M861%2C606Q792%2C712%2C711.5%2C808.5Q631%2C905%2C505%2C889.5Q379%2C874%2C279.5%2C803Q180%2C732%2C144%2C616Q108%2C500%2C151%2C389Q194%2C278%2C293%2C222.5Q392%2C167%2C513%2C126.5Q634%2C86%2C721.5%2C180.5Q809%2C275%2C869.5%2C387.5Q930%2C500%2C861%2C606Z%22%3E%3C%2Fpath%3E %3C%2FclipPath%3E %3C%2Fdefs%3E %3Cg clip-path=%22url(%23shape)%22%3E %3Cpath fill=%22url(%23radialGradientId)%22 d=%22M861%2C606Q792%2C712%2C711.5%2C808.5Q631%2C905%2C505%2C889.5Q379%2C874%2C279.5%2C803Q180%2C732%2C144%2C616Q108%2C500%2C151%2C389Q194%2C278%2C293%2C222.5Q392%2C167%2C513%2C126.5Q634%2C86%2C721.5%2C180.5Q809%2C275%2C869.5%2C387.5Q930%2C500%2C861%2C606Z%22 %2F%3E %3C%2Fg%3E %3C%2Fsvg%3E");
+`;
+
+export const StudentImage = styled.img`
+position: absolute;
+`;
